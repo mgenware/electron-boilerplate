e537d89cbb537b734ddc5211c7ab7304ffd6249a
Mgen <m@mgenware.com>
2020-11-21 14:34:38 +0800

    Switch to original rollup-plugin-lit-css as the issue's been fixed in upstream

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import nodeResolve from '@rollup/plugin-node-resolve';
 import json from '@rollup/plugin-json';
 import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
-import litcss from 'rollup-plugin-lit-css-ex';
+import litcss from 'rollup-plugin-lit-css';
 import autoExternal from 'rollup-plugin-auto-external';
 
 const isProd = process.env.NODE_ENV == 'production';
